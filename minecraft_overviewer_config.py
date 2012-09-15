0dede5630c76e2fbb943935dbe7cc0e4796017f6
Alexander Surma <surma@surmair.de>
2012-09-16 01:43:43 +0200

    :Minecraver overviewer config

--- minecraft_overviewer_config.py
+++ minecraft_overviewer_config.py
@@ -3,35 +3,35 @@ worlds["Nether"] = "world_nether"
 worlds["The End"] = "world_the_end"
 
 processes = 2
-outputdir = "rendering2"
+outputdir = "rendering"
 texturepath = "/root/dokucraft.zip"
 
 # Overworld
 renders["overworld_day_north"] = {
 	"world": "Overworld",
 	"title": "North",
-	"rendermode": "smooth_lighting",
+	"rendermode": "normal",
 	"northdirection": "upper_left",
 }
 
 renders["overworld_day_east"] = {
 	"world": "Overworld",
 	"title": "East",
-	"rendermode": "smooth_lighting",
+	"rendermode": "normal",
 	"northdirection": "lower_left",
 }
 
 renders["overworld_day_south"] = {
 	"world": "Overworld",
 	"title": "South",
-	"rendermode": "smooth_lighting",
+	"rendermode": "normal",
 	"northdirection": "lower_right",
 }
 
 renders["overworld_day_west"] = {
 	"world": "Overworld",
 	"title": "West",
-	"rendermode": "smooth_lighting",
+	"rendermode": "normal",
 	"northdirection": "upper_right",
 }
 
@@ -68,28 +68,57 @@ renders["overworld_cave_west"] = {
 renders["nether_north"] = {
 	"world": "Nether",
 	"title": "North",
-	"rendermode": "nether_smooth_lighting",
+	"rendermode": "nether",
 	"northdirection": "upper_left",
 }
 
-renders["nether_smooth_lighting_east"] = {
+renders["nether_east"] = {
 	"world": "Nether",
 	"title": "East",
-	"rendermode": "nether_smooth_lighting",
+	"rendermode": "nether",
 	"northdirection": "lower_left",
 }
 
-renders["nether_smooth_lighting_south"] = {
+renders["nether_south"] = {
 	"world": "Nether",
 	"title": "South",
-	"rendermode": "nether_smooth_lighting",
+	"rendermode": "nether",
 	"northdirection": "lower_right",
 }
 
-renders["nether_smooth_lighting_west"] = {
+renders["nether_west"] = {
 	"world": "Nether",
 	"title": "West",
-	"rendermode": "nether_smooth_lighting",
+	"rendermode": "nether",
+	"northdirection": "upper_right",
+}
+
+# Nether Cave
+renders["cave_north"] = {
+	"world": "Nether",
+	"title": "Cave North",
+	"rendermode": "cave",
+	"northdirection": "upper_left",
+}
+
+renders["cave_east"] = {
+	"world": "Nether",
+	"title": "Cave East",
+	"rendermode": "cave",
+	"northdirection": "lower_left",
+}
+
+renders["cave_south"] = {
+	"world": "Nether",
+	"title": "Cave South",
+	"rendermode": "cave",
+	"northdirection": "lower_right",
+}
+
+renders["cave_west"] = {
+	"world": "Nether",
+	"title": "Cave West",
+	"rendermode": "cave",
 	"northdirection": "upper_right",
 }
 
@@ -97,27 +126,56 @@ renders["nether_smooth_lighting_west"] = {
 renders["end_north"] = {
 	"world": "The End",
 	"title": "North",
-	"rendermode": "smooth_lighting",
+	"rendermode": "normal",
 	"northdirection": "upper_left",
 }
 
 renders["end_east"] = {
 	"world": "The End",
 	"title": "East",
-	"rendermode": "smooth_lighting",
+	"rendermode": "normal",
 	"northdirection": "lower_left",
 }
 
 renders["end_south"] = {
 	"world": "The End",
 	"title": "South",
-	"rendermode": "smooth_lighting",
+	"rendermode": "normal",
 	"northdirection": "lower_right",
 }
 
 renders["end_west"] = {
 	"world": "The End",
 	"title": "West",
-	"rendermode": "smooth_lighting",
+	"rendermode": "normal",
+	"northdirection": "upper_right",
+}
+
+# The End Cave
+renders["end_north"] = {
+	"world": "The End",
+	"title": "Cave North",
+	"rendermode": "cave",
+	"northdirection": "upper_left",
+}
+
+renders["end_east"] = {
+	"world": "The End",
+	"title": "Cave East",
+	"rendermode": "cave",
+	"northdirection": "lower_left",
+}
+
+renders["end_south"] = {
+	"world": "The End",
+	"title": "Cave South",
+	"rendermode": "cave",
+	"northdirection": "lower_right",
+}
+
+renders["end_west"] = {
+	"world": "The End",
+	"title": "Cave West",
+	"rendermode": "cave",
 	"northdirection": "upper_right",
 }
